3229ae4e58115f8855af15b1e22ccf8506a8c62a
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2024-05-06 15:18:14 -0400

    setup relay client

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,12 +1,12 @@
 import "./global.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { SvixProvider } from "svix-react";
+import { RelayEnvironmentProvider } from "react-relay";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
-
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 import { ThemeProvider } from "./components/theme-provider";
+import { environment } from "./relay";
 
 // Create a new router instance
 const router = createRouter({ routeTree });
@@ -20,16 +20,10 @@ declare module "@tanstack/react-router" {
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <SvixProvider
-      token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MTI2ODU4MjAsImV4cCI6MTcxNTEwNTAyMCwibmJmIjoxNzEyNjg1ODIwLCJpc3MiOiJzdml4LXNlcnZlciIsInN1YiI6ImFwcF8yZXNHRUoyR0MzWGdLc2Z4d2xNY3dtVGV1RDciLCJvcmciOiJvcmdfMjNyYjhZZEdxTVQwcUl6cGdHd2RYZkhpck11In0.oVtaK6aGeCjGOkjIv3EN0Ams4oALs6A5TUn1xarZ5x8"
-      appId="app_2esGEJ2GC3XgKsfxwlMcwmTeuD7"
-      options={{
-        serverUrl: "http://localhost:8071",
-      }}
-    >
+    <RelayEnvironmentProvider environment={environment}>
       <ThemeProvider defaultTheme="dark" storageKey="graph-webhooks">
         <RouterProvider router={router} />
       </ThemeProvider>
-    </SvixProvider>
+    </RelayEnvironmentProvider>
   </React.StrictMode>
 );
---
+++ web/src/relay.ts
@@ -0,0 +1,82 @@
+import { GraphQLError, GraphQLErrorOptions } from "graphql";
+import {
+  Environment,
+  Network,
+  Observable,
+  RecordSource,
+  Store,
+} from "relay-runtime";
+
+if (!import.meta.env.VITE_GRAPHQL_ENDPOINT) {
+  throw new Error("VITE_GRAPHQL_ENDPOINT is not defined");
+}
+
+export const environment = new Environment({
+  store: new Store(new RecordSource()),
+  network: Network.create((operation, variables) => {
+    return Observable.create((sink) => {
+      const ctrl = new AbortController();
+
+      (async () => {
+        const res = await fetch(
+          // include the `operation.operationKind` and `operation.name` because its easier to navigate through the GraphQL requests in the DevTools
+          `${import.meta.env.VITE_GRAPHQL_ENDPOINT}?${operation.operationKind}=${operation.name}`,
+          {
+            signal: ctrl.signal,
+            method: "POST",
+            credentials: "include",
+            headers: {
+              "content-type": "application/json; charset=utf-8",
+            },
+            body: JSON.stringify({
+              operationName: operation.name,
+              query: operation.text || "",
+              variables,
+            }),
+          }
+        );
+
+        if (!res.ok) {
+          const err = new Error((await res.text()) || res.statusText);
+          err.name = "GraphQLRequestError";
+          throw err;
+        }
+
+        const data = await res.json();
+
+        if (data.errors) {
+          const errs = data.errors.map(
+            ({
+              message,
+              ...opts
+            }: GraphQLErrorOptions & { message: string }) => {
+              return new GraphQLError(message, opts);
+            }
+          );
+          if (errs.length === 1) {
+            throw errs[0];
+          }
+          const aggErr = new AggregateError(errs);
+          aggErr.name = "GraphQLAggregateError";
+          aggErr.toString = () =>
+            `${aggErr.name}: ${aggErr.errors.map((err) => err.message).join(", ")}`;
+          throw aggErr;
+        }
+
+        return data;
+      })()
+        .then(sink.next)
+        .then(sink.complete)
+        .catch(sink.error);
+
+      return () => {
+        ctrl.abort();
+      };
+    });
+  }),
+});
+
+if (import.meta.env.DEV) {
+  // @ts-expect-error for debugging purposes
+  window.relayEnvironment = environment;
+}
